31151b8dfeee328cdc64ba89e0ffca00042652cc
yukilayman <yukilayman@gmail.com>
2023-08-02 13:17:39 -0400

    Initial Commit

---
+++ learn stock sensei/learn_stock_senseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_stock_senseiApp.swift
+//  learn stock sensei
+//
+//  Created by scholar on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct learn_stock_senseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
